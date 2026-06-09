0fb26ff86cae6b4bf3bbb77cdf4824919d08b14e
hube0001 <hube0001@stud.kea.dk>
2026-06-09 10:04:09 +0200

    Initialize kat-recent-blogs-start

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Hero from "@/components/forside/Hero";
+import NightclubGallery from "@/components/forside/NightclubGallery";
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <NightclubGallery />
+    </div>
+  );
+}
