b2312e28bdc9fce5b36d4bd40a22288270572314
hube0001 <hube0001@stud.kea.dk>
2026-06-09 10:05:31 +0200

    Initialize alma-fokus-state-opdater

---
+++ src/app/page.js
@@ -0,0 +1,37 @@
+import Hero from "@/components/forside/Hero";
+import GalleryData from "@/components/forside/GalleryData";
+import BurgerMenu from "@/components/Burger";
+import NavBar from "@/components/Navbar";
+import WelcomeImg from "@/components/forside/WelcomeImg";
+import FeautredEvents from "@/components/forside/FeautredEvents";
+import Track from "@/components/forside/Track";
+import Video from "@/components/forside/Video";
+import Testimonials from "@/components/forside/Testimonials";
+import Newsletter from "@/components/forside/Nyhedsbrev";
+import { Suspense } from "react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center py-20">
+            <Image src="/assets/loader/madbars.gif" alt="Henter..." width={80} height={80} unoptimized />
+          </div>
+        }
+      >
+        <Hero />
+      </Suspense>
+      <NavBar />
+      <BurgerMenu />
+      <WelcomeImg />
+      <FeautredEvents />
+      <GalleryData />
+      <Track />
+      <Video />
+      <Testimonials />
+      <Newsletter />
+    </div>
+  );
+}
