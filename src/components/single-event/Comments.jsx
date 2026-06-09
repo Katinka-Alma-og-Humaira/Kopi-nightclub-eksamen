217f42e7ab63a59555fb07824db5ba283e413e65
hube0001 <hube0001@stud.kea.dk>
2026-06-09 10:05:15 +0200

    Initialize fælles-banner

---
+++ src/components/single-event/Comments.jsx
@@ -0,0 +1,47 @@
+import { cacheLife } from "next/cache";
+
+async function getComments(eventId) {
+  "use cache";
+  cacheLife("hours");
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/comments`, {
+    next: { tags: ["comments"] },
+  });
+  const allComments = await response.json();
+  return allComments.filter((comment) => comment.eventId === eventId);
+}
+
+const Comments = async ({ eventId }) => {
+  const comments = await getComments(eventId);
+
+  return (
+    <div>
+      <h1 className="mb-(--space-s)">{comments.length} Comments</h1>
+
+      {comments.length === 0 ? (
+        <p>No comments yet. Be the first to comment!</p>
+      ) : (
+        comments.map((comment) => (
+          <div key={comment.id}>
+            <h3>
+              <span>{comment.name} - </span>
+              <span className="text-(--color-pink) text-h4">
+                Posted{" "}
+                {new Date(comment.date)
+                  .toLocaleDateString("en-EN", {
+                    day: "2-digit",
+                    month: "long",
+                    year: "numeric",
+                  })
+                  .replace(/\b\w/, (c) => c.toUpperCase())}
+              </span>
+            </h3>
+            <p className="text-base!">{comment.content}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Comments;
