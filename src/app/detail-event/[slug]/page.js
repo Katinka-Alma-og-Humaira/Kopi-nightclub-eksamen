e782aee04e00172777a975039e1b7f551725b78c
KatKhir <kasa0004@stud.kea.dk>
2026-06-10 22:57:23 +0200

    single event fall virker

--- src/app/detail-event/[slug]/page.js
+++ src/app/detail-event/[slug]/page.js
@@ -2,10 +2,17 @@ import { Suspense } from "react";
 import { EventContent } from "@/components/single-event/EventContent";
 import NavBar from "@/components/Navbar";
 import BurgerMenu from "@/components/Burger";
+import Image from "next/image";
 const Page = ({ params }) => {
   return (
     <div>
-      <Suspense fallback={<div>Loading event...</div>}>
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center py-20">
+            <Image src="/assets/loader/madbars.gif" alt="Loading..." width={80} height={80} unoptimized />
+          </div>
+        }
+      >
         <NavBar />
         <BurgerMenu />
         <EventContent params={params} />
